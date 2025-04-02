c9f25739861e0c1a6e03561ece4b58f0db3569e8
Babu Macharia <babu.macharia@gmail.com>
2025-04-02 12:41:11 -0700

    feat: boto3 S3 library, autocompletion

--- run.sh
+++ run.sh
@@ -16,8 +16,8 @@ MINIMUM_TEST_COVERAGE_PERCENT=0
 
 # install core and development Python dependencies into the currently activated venv
 function install {
-    python -m pip install --upgrade pip
-    python -m pip install --editable "$THIS_DIR/[dev]"
+    python3 -m pip install --upgrade pip
+    python3 -m pip install --editable "$THIS_DIR/[dev]"
 }
 
 # run linting, formatting, and other static code quality tools
