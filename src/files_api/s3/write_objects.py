d15244936c38ff8e2a78ae05bbf17cb79fff32fd
Babu Macharia <babu.macharia@gmail.com>
2025-04-14 13:35:38 -0700

    feat: upload_function implemented

---
+++ src/files_api/s3/write_objects.py
@@ -0,0 +1,46 @@
+"""Functions for writing objects from an S3 bucket--the "C" and "U" in CRUD.
+
+wrap the boto3 S3 client put_object function
+
+first create an S3 client (note: the function can accept an existing client instead of instantiating a new one every time)
+
+
+"""
+
+from typing import Optional
+
+import boto3
+
+try:
+    from mypy_boto3_s3 import S3Client
+except ImportError:
+    ...
+
+
+def upload_s3_object(
+    bucket_name: str,
+    object_key: str,
+    file_content: bytes,
+    content_type: Optional[str] = None,
+    s3_client: Optional["S3Client"] = None,
+) -> None:
+    """
+    Upload a file to an S3 bucket.
+
+    :param bucket_name: The name of the S3 bucket.
+    :param object_key: path to the object in the S3 bucket.
+    :param file_content: The content of the file to upload.
+    :param content_type: The MIME type of the file, e.g. "text/plain" for a text file.
+    :param s3_client: An optional boto3 S3 client. If not provided, one will be created.
+    """
+    # s3 clinet can be the one passed in or the function intializes a new one
+    s3_client = s3_client or boto3.client("s3")
+    content_type = content_type or "application/octet-stream"
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=object_key,
+        Body=file_content,
+        ContentType=content_type,
+    )
+
+    return
